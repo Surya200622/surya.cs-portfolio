882103f10df81ef9ecf864b609d66fcda8c94795
Surya <cssurya2006@gmail.com>
2026-03-27 10:44:19 +0530

    Change 'Surya' to 'SURYA' in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
     >
       <div className="max-w-7xl mx-auto px-6 flex justify-between items-center relative">
         <Link to="/" className="text-2xl font-bold text-white tracking-widest text-shadow-sm">
-          Surya<span className="text-neon-blue">.CS</span>
+          SURYA<span className="text-neon-blue">.CS</span>
         </Link>
         
         {/* Desktop Menu */}
